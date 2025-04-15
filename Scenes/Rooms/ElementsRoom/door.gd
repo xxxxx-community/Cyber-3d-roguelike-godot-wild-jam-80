27acc8ebf9e679b013671a34733740901a65fe02
SamuelRodriguesMGR <marat78880@gmail.com>
2025-04-15 23:41:25 +0500

    система уровней

---
+++ Scenes/Rooms/ElementsRoom/door.gd
@@ -0,0 +1,7 @@
+extends StaticBody3D
+
+
+func _on_player_detector_body_entered(body: Node3D) -> void:
+	if body.name == "Player":
+		$PlayerDetector.queue_free()
+		$AnimationPlayer.play(&"open")
---
+++ Scenes/Rooms/spawn_rooms.gd
@@ -0,0 +1,44 @@
+extends Node3D
+
+
+const SPAWN_ROOMS        : Array[PackedScene] = [
+	preload("res://Scenes/Rooms/SpawnRooms/spawn_room_1.tscn")
+	]
+const INTERMEDIATE_ROOMS : Array[PackedScene] = [
+	preload("res://Scenes/Rooms/IntermediateRooms/intermediate_room_1.tscn"),
+	preload("res://Scenes/Rooms/IntermediateRooms/intermediate_room_2.tscn")
+	]
+
+var number_levels : int = 100
+
+@onready var player : CharacterBody3D = get_tree().current_scene.get_node("Player")
+
+func _ready():
+	_spawn_rooms()
+
+func _spawn_rooms():
+	var previous_room : Node3D
+	
+	for i in number_levels:
+		var room : Node3D
+		
+		if i == 0:
+			room = SPAWN_ROOMS.pick_random().instantiate()
+			player.position = room.get_node("PlayerSpawnPoint").position
+			
+		else:
+			# обычная комната
+			room = INTERMEDIATE_ROOMS.pick_random().instantiate() 
+			
+			var grid_map_prev_room : GridMap = previous_room.get_node("GridMap") 
+			var door_prev_room : StaticBody3D = previous_room.get_node("Door")
+			var exit_pos  : Vector3i = grid_map_prev_room.local_to_map(door_prev_room.global_position) - Vector3i.UP
+			
+			var grid_map_cur_room : GridMap = room.get_node("GridMap") 
+			var connect_cur_room : Marker3D = room.get_node("ConnectingPassage")
+			var connect_passage_pos : Vector3i = grid_map_cur_room.local_to_map(connect_cur_room.position) - Vector3i.UP
+			
+			room.position = exit_pos - connect_passage_pos
+			
+		add_child(room)
+		previous_room = room
