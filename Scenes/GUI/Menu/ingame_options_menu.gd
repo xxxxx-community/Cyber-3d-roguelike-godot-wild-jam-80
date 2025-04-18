78845045633adabba12f8bb72f8b2dcae09a30e1
AlexonMorris <goodbad26262626@yandex.ru>
2025-04-18 09:07:20 +0300

    menu sound effects

--- Scenes/GUI/Menu/ingame_options_menu.gd
+++ Scenes/GUI/Menu/ingame_options_menu.gd
@@ -7,15 +7,19 @@ func _ready() -> void:
 	%FullscreenButton.button_pressed = SettingsData.fullscreen
 
 func _on_button_pressed() -> void:
+	$button1.play()
 	get_tree().quit()
 
 func _on_volume_value_changed(value: float) -> void:
+	$button2.play()
 	SettingsData.volume = value
 
 func _on_mute_toggled(toggled_on: bool) -> void:
+	$button2.play()
 	SettingsData.mute = toggled_on
 
 func _on_fullscreen_button_toggled(button_pressed: bool) -> void:
+	$button3.play()
 	SettingsData.fullscreen = button_pressed
 
 func _on_volume_drag_ended(value_changed: bool) -> void:
--- Scenes/GUI/Menu/options_menu.gd
+++ Scenes/GUI/Menu/options_menu.gd
@@ -7,16 +7,20 @@ func _ready() -> void:
 	%FullscreenButton.button_pressed = SettingsData.fullscreen
 
 func _on_volume_value_changed(value: float) -> void:
+	$button2.play()
 	SettingsData.volume = value
 
 func _on_mute_toggled(toggled_on: bool) -> void:
+	$button2.play()
 	SettingsData.mute = toggled_on
 
 func _on_fullscreen_button_toggled(button_pressed: bool) -> void:
+	$button3.play()
 	SettingsData.fullscreen = button_pressed
 
 # Остальной код без изменений
 func _on_back_button_pressed() -> void:
+	$button1.play()
 	%CamPlayer.play("cam_main")
 
 func _on_cam_player_animation_finished(anim_name: StringName) -> void:
