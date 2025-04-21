1622a9321d50efdf43783e185dc803914ed5dc3f
SamuelRodriguesMGR <marat78880@gmail.com>
2025-04-21 23:51:05 +0500

    кастсцена

--- Scenes/GUI/Menu/menu.gd
+++ Scenes/GUI/Menu/menu.gd
@@ -4,6 +4,9 @@ var scene = preload("res://Scenes/GUI/Menu/opt_menu_room.tscn")
 
 func _on_play_button_pressed() -> void:
 	$button1.play()
+	$"../AnimationPlayer".play(&"story")
+	
+func start_game() -> void:
 	get_tree().change_scene_to_file("res://Scenes/game.tscn")
 
 
